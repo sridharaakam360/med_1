7542b4c1a2de8a09c09926af5fb6652c90ae41eb
sridhar <sridhar@shanmugha.edu.in>
2025-05-07 18:57:01 +0000

    updated init.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,19 @@
 import os
-from flask import Flask
-from .models import db
+from flask import Flask, redirect, url_for
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'dev')
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.sqlite'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-    db.init_app(app)
-
-    with app.app_context():
-        db.create_all()
+    app.config['DATABASE'] = os.path.join(app.root_path, 'data.sqlite')
 
     from .billing import billing_bp
     from .inventory import inventory_bp
+
     app.register_blueprint(billing_bp)
     app.register_blueprint(inventory_bp)
+
+    @app.route("/")
+    def index():
+        return redirect(url_for("billing_bp.billing"))
 
     return app
